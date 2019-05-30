a0ad607b1059ec27e683eb3be60c4e8ef02d6cd6
Артур <artur8888b@gmail.com>
2019-05-30 21:09:39 +0300

    Проект для защиты #2

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,7 +3,8 @@ var mobileMenu = document.querySelector(".header__nav-menu"),
   mapPicture = document.querySelector(".map"),
   form = document.querySelector(".form"),
   textfields = document.querySelectorAll(".form-field--required"),
-  btnTitle = document.querySelector(".header__menu-button-title");
+  btnTitle = document.querySelector(".header__menu-button-title"),
+  header = document.querySelector(".header");
 
 mobileMenu.classList.add("header__nav-menu--hidden");
 btnMenu.classList.add("header__menu-button--open");
@@ -15,11 +16,11 @@ btnMenu.addEventListener("click", function (evt) {
   if (mobileMenu.classList.contains("header__nav-menu--hidden")) {
     mobileMenu.classList.remove("header__nav-menu--hidden");
     btnTitle.innerText = "Закрыть меню";
+    header.style = "border-bottom:none";
   }
   else {
     mobileMenu.classList.add("header__nav-menu--hidden");
     btnTitle.innerText = "Открыть меню";
+
   }
 });
-
-
